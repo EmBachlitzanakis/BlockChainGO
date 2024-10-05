1eb3b941abd5f3fe9e95b678f70d9416b4d5284b
Emmanuel Bachlitzanakis <115871174+EmBachlitzanakis@users.noreply.github.com>
2024-10-05 22:25:46 +0300

    05/10/2024

---
+++ BlockchainGo/blockchain/blockchain.go
@@ -0,0 +1,20 @@
+package blockchain
+
+import (
+    "bytes"
+    "encoding/gob"
+)
+type BlockChain struct {
+	Blocks []*Block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, new)
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
